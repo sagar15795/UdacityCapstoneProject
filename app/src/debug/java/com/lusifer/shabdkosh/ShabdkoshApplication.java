27081bae5316e25d9fb39bf1e3aaec3f7712c2c5
Sagar <kumarsagar15795@gmail.com>
2017-02-27 13:49:02 +0530

    feat : added debug and release build variant

---
+++ app/src/debug/java/com/lusifer/shabdkosh/ShabdkoshApplication.java
@@ -0,0 +1,30 @@
+package com.lusifer.shabdkosh;
+
+import com.facebook.stetho.Stetho;
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowLog;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+import android.app.Application;
+import android.content.Context;
+
+
+public class ShabdkoshApplication extends Application {
+
+    private static ShabdkoshApplication instance;
+
+    public static Context getContext() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (instance == null) {
+            instance = this;
+        }
+        Stetho.initializeWithDefaults(this);
+        FlowManager.init(new FlowConfig.Builder(this).build());
+        FlowLog.setMinimumLoggingLevel(FlowLog.Level.V);
+    }
+}
--- app/src/release/java/com/lusifer/shabdkosh/ShabdkoshApplication.java
+++ app/src/release/java/com/lusifer/shabdkosh/ShabdkoshApplication.java
@@ -1,6 +1,5 @@
 package com.lusifer.shabdkosh;
 
-import com.facebook.stetho.Stetho;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowLog;
 import com.raizlabs.android.dbflow.config.FlowManager;
@@ -23,8 +22,6 @@ public class ShabdkoshApplication extends Application {
         if (instance == null) {
             instance = this;
         }
-
-        Stetho.initializeWithDefaults(this);
         FlowManager.init(new FlowConfig.Builder(this).build());
         FlowLog.setMinimumLoggingLevel(FlowLog.Level.V);
     }
