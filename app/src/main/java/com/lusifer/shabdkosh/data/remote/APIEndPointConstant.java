181dad7a82bfc7f379f609fc3f34cd5a30c5caad
Sagar <kumarsagar15795@gmail.com>
2017-03-12 22:45:29 +0530

    feat: added api service url

--- app/src/main/java/com/lusifer/shabdkosh/data/remote/APIEndPointConstant.java
+++ app/src/main/java/com/lusifer/shabdkosh/data/remote/APIEndPointConstant.java
@@ -3,4 +3,6 @@ package com.lusifer.shabdkosh.data.remote;
 
 class APIEndPointConstant {
 
+    public static final String PATH_WORD = "word";
+    public static final String WORDS = "words/{" + PATH_WORD + "}";
 }
