e7326501382463493ae64a1609bd0d2647950609
Sagar <kumarsagar15795@gmail.com>
2017-03-18 22:56:21 +0530

    feat : added contentProvider

--- app/src/main/java/com/lusifer/shabdkosh/data/local/ShabdkoshDatabase.java
+++ app/src/main/java/com/lusifer/shabdkosh/data/local/ShabdkoshDatabase.java
@@ -1,13 +1,20 @@
 package com.lusifer.shabdkosh.data.local;
 
 import com.raizlabs.android.dbflow.annotation.Database;
+import com.raizlabs.android.dbflow.annotation.provider.ContentProvider;
 
-
+@ContentProvider(authority = ShabdkoshDatabase.AUTHORITY,
+        database = ShabdkoshDatabase.class,
+        baseContentUri = ShabdkoshDatabase.BASE_CONTENT_URI)
 @Database(name = ShabdkoshDatabase.NAME, version = ShabdkoshDatabase.VERSION)
 public class ShabdkoshDatabase {
 
     public static final String NAME = "ShabdkoshDatabase";
 
     public static final int VERSION = 1;
+
+    public static final String AUTHORITY = "com.lusifer.shabdkosh";
+
+    public static final String BASE_CONTENT_URI = "content://";
 
 }
